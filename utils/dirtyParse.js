2cf97b1ff167c5b65598c7fadd9b11fc8599c0df
Zhu Chu <mystery490815101@gmail.com>
2017-07-21 20:59:26 +0800

    use eval to prevent problems converting unformatted json

--- utils/dirtyParse.js
+++ utils/dirtyParse.js
@@ -1,3 +1,4 @@
 module.exports = str => {
-  return JSON.parse(str.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": '));
+  // return JSON.parse(str.replace(/(['"])?([a-z0-9A-Z_]+)(['"])?:/g, '"$2": '));
+  return eval(`(${str})`); // now use evil eval
 };
